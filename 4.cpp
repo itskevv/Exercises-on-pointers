40a0a65ab9ef4e1b85ff76eeea826573501c8676
Kevin <106379693+itskevv@users.noreply.github.com>
2023-01-27 05:29:56 +0200

    Update 4.cpp

--- 4.cpp
+++ 4.cpp
@@ -2,31 +2,34 @@
 using namespace std;
 int main()
 {
-	int arr[100],i,odd,even,s_odd=0,s_even=0;
+	int Numbers[100],i,odd,even,s_odd=0,s_even=0;
 	cout<<"Enter 15 numbers: ";
 	for(i=0;i<15;i++)
 	{
-		cin>>arr[i];
+		cin>>Numbers[i];
 	}
+	int *arr[100];
+	arr[100]=&Numbers[100];
+	
 	cout<<"Even numbers: "<<endl;
 	for(i=0;i<15;i++)
 	{
-		if(arr[i]%2==0)
+		if(*arr[i]%2==0)
 		{
-			cout<<" "<<arr[i];
-			s_even=s_even+arr[i];
+			cout<<" "<<*arr[i];
+			s_even=s_even+*arr[i];
 		}
 	}
 	cout<<endl<<"Odd numbers: "<<endl;
 	for(i=0;i<15;i++)
 	{
-		if(arr[i]%2!=0)
+		if(*arr[i]%2!=0)
 		{
-			cout<<" "<<arr[i];
-			s_odd=s_odd+arr[i];
+			cout<<" "<<*arr[i];
+			s_odd=s_odd+*arr[i];
 		}
 	}
-	
+
 	cout<<endl<<"Sum of even numbers: "<<s_even<<endl;
 	cout<<"Sum of odd numbers: "<<s_odd<<endl;
 }
